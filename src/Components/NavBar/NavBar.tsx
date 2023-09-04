e5977bf2714f0f15277b6234c6629dd81fbd6cff
sssromaz <sssromaz@mail.ru>
2023-09-04 13:22:42 +0300

    navBar

--- src/Components/NavBar/NavBar.tsx
+++ src/Components/NavBar/NavBar.tsx
@@ -1,10 +1,113 @@
 import React from 'react';
+import styles from './NavBar.module.css'
+import logo from '../../images/logo.svg'
+import { DownOutlined } from '@ant-design/icons';
+import type {MenuProps} from 'antd';
+import { Dropdown, Space } from 'antd';
 
 export const NavBar = () => {
   return (
-    <div>
-
-    </div>
+    <header className={styles.navbar}>
+      <div className={styles.container}>
+        <div className={styles.logo}>
+          <img src={logo} alt="logo"/>
+        </div>
+        <Dropdown menu={{ items }}>
+          <a onClick={(e) => e.preventDefault()}>
+            <Space className={styles.navButtons}>
+              Услуги
+              <DownOutlined />
+            </Space>
+          </a>
+        </Dropdown>
+        <Space className={styles.navButtons}>
+          Обо мне
+        </Space>
+        <Space className={styles.navButtons}>
+          Контакты
+        </Space>
+      </div>
+    </header>
   );
 };
+
+
+const items: MenuProps['items'] = [
+
+  {
+    key: '1',
+    label: 'Инъекционная косметология',
+    children: [
+      {
+        key: '1-1',
+        label: 'Консультация',
+      },
+      {
+        key: '1-2',
+        label: 'Биоревитализация и мезотерапия',
+      },
+      {
+        key: '1-3',
+        label: 'Коллагенотерапия',
+      },
+      {
+        key: '1-4',
+        label: 'Увеличение губ',
+      },
+      {
+        key: '1-5',
+        label: 'Растворение старого филлера',
+      },
+      {
+        key: '1-6',
+        label: 'Ферментотерапия и липолитики',
+      },
+    ],
+  },
+  {
+    key: '2',
+    label: 'Безинъекционная косметология',
+    children: [
+      {
+        key: '2-1',
+        label: 'Консультация',
+      },
+      {
+        key: '2-2',
+        label: 'Разбор/подбор домашнего ухода',
+      },
+      {
+        key: '2-3',
+        label: 'Уход для кожы с акне',
+      },
+      {
+        key: '2-4',
+        label: 'Антивозрастной уход',
+      },
+      {
+        key: '2-5',
+        label: 'Уход за кожей с пигментацией',
+      },
+      {
+        key: '2-6',
+        label: 'Уход для чувствительной кожей',
+      },
+      {
+        key: '2-7',
+        label: 'Экспресс лифтинг',
+      },
+      {
+        key: '2-8',
+        label: 'увлажнение и антиоксидантный уход',
+      },
+    ],
+  },
+  {
+    key: '3',
+    label: 'Профессиональная косметика',
+
+  },
+
+];
+
 
---
+++ src/constants/navbarButtons.ts
@@ -0,0 +1,29 @@
+import {navbarButtonType} from "../types/appTypes";
+
+export const navbarButtons: navbarButtonType[] = [
+  {
+    id: 1,
+    title: 'Инъекционная косметология',
+    path: '',
+  },
+  {
+    id: 2,
+    title: 'Безинъекционная косметология',
+    path: '',
+  },
+  {
+    id: 3,
+    title: 'Профессиональная косметика',
+    path: '',
+  },
+  /*{
+    id: 4,
+    title: 'Обо мне',
+    path: '',
+  },
+  {
+    id: 5,
+    title: 'Контакты',
+    path: '',
+  },*/
+];
